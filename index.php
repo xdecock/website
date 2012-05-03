4a8868e070fcdeb3714899d6c5a28cca323ebfb0
xdecock <xdecock@T420s>
2012-05-03 12:47:49 +0200

    Correction varname typo
    Change-Id: I4cb5b049cd3f2940b128b50622328787ffa9de9b

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $contentFile=dirname(__FILE__).'/content/'.$pageName;
 $existsPHP = file_exists($contentFile.'.php');
 $existsTxt = false;
 if (!$existsPHP) {
-	$exist_txt = file_exists($contentFile.'.txt');
+	$existTxt = file_exists($contentFile.'.txt');
 } 
 if (!$existsTxt && !$existsPHP) {
 	// 404
